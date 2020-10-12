cb2a14f910989348c6e1a7235f6d68093c802b09
Bojan Vrhovnik <bojan@vrhovnik.net>
2020-10-12 22:19:11 +0200

    changing model, because AWS send models

--- src/LambadaInc/Lambada.DataGenerators/AwsHubEventHub.cs
+++ src/LambadaInc/Lambada.DataGenerators/AwsHubEventHub.cs
@@ -18,17 +18,26 @@ namespace LambadaInc.Generators
             [CosmosDB(databaseName: "lambadadb", collectionName: "stats",
                 ConnectionStringSetting = "GenerateOptions:CosmosDbConnectionString")]
             IAsyncCollector<FactoryStatModel> stats,
-            [SignalR(HubName = "messages",ConnectionStringSetting = "AzureSignalRConnectionString")] IAsyncCollector<SignalRMessage> signalRMessages,
+            [SignalR(HubName = "messages", ConnectionStringSetting = "AzureSignalRConnectionString")]
+            IAsyncCollector<SignalRMessage> signalRMessages,
             ILogger log)
         {
-            log.LogInformation($"Processing message from truck event hub, enqueued at {truckMobileMessage.SystemProperties.EnqueuedTimeUtc}");
+            log.LogInformation(
+                $"Processing message from truck event hub, enqueued at {truckMobileMessage.SystemProperties.EnqueuedTimeUtc}");
             var message = Encoding.UTF8.GetString(truckMobileMessage.Body);
 
-            var awsMessage = JsonConvert.DeserializeObject<AwsMessage>(message);
+            log.LogInformation(message);
 
-            var moneyExpected = awsMessage.BottleCount * 2.5f;
-            log.LogInformation($"Expected money processed {moneyExpected}");
+            var awsMessage = JsonConvert.DeserializeObject<AwsMessage>(message);
+            log.LogInformation($"Message source {awsMessage.Source} with message");
+            log.LogInformation($"Cargo {awsMessage.Message.Payload.Cargo}");
+            log.LogInformation($"Humidity {awsMessage.Message.Payload.Humidity}");
+            log.LogInformation($"Temp {awsMessage.Message.Payload.Temperature}");
+            log.LogInformation($"Truck ID {awsMessage.Message.Payload.TruckId}");
             
+            var moneyExpected = awsMessage.Message.Payload.BottleCount * 2.5f;
+            log.LogInformation($"Expected money processed {moneyExpected}");
+
             //currently only one Factory has been aquired as trucks are assigned to them
             await stats.AddAsync(new FactoryStatModel
             {
@@ -36,7 +45,7 @@ namespace LambadaInc.Generators
                 DateCreated = DateTime.Now,
                 EarnedMoney = moneyExpected
             });
-            
+
             string showMessage = $"I received message from AWS truck and earned {moneyExpected}";
             log.LogInformation(showMessage);
             await signalRMessages.AddAsync(
@@ -48,8 +57,23 @@ namespace LambadaInc.Generators
         }
     }
 
-    public class AwsMessage
-    {
-        public int BottleCount { get; set; }
+    public class PayloadMessage    {
+        public string TruckId { get; set; } 
+        public string Cargo { get; set; } 
+        public int BottleCount { get; set; } 
+        public string Temperature { get; set; } 
+        public string Humidity { get; set; } 
+    }
+
+    public class Message    {
+        [JsonProperty("message")]
+        public PayloadMessage Payload { get; set; } 
+    }
+
+    public class AwsMessage    {
+        [JsonProperty("source")]
+        public string Source { get; set; }
+        [JsonProperty("message")]
+        public Message Message { get; set; } 
     }
 }
